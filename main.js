dfa68a2c28893aa3c4df0422ed2bad7bd7f34b9e
pavlosantoniou13 <dinant11133@gmail.com>
2023-01-17 23:22:03 +0200

    second stage before adding the flair basicly or fuctionality is in

--- main.js
+++ main.js
@@ -6,12 +6,28 @@ const player1 = Player("Player X", "X")
 const player2 = Player("Player O", "O")
 
 const gameBoardObject = (() => {
-    let gameBoard = []
-    for (i=0; i<9; i++) {
-        gameBoard.push("")
-    }
-    
-})
+    let gameBoard = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ]
+    gameBoard.forEach(game => {
+        game
+    })
+   return {gameBoard}
+})();
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 
 const container = document.querySelector(".board");
 function makeRows(rows, cols) {
@@ -20,29 +36,52 @@ function makeRows(rows, cols) {
   
   
     for (c = 0; c < (rows * cols); c++) {
+        
       let cell = document.createElement("div");
       cell.id = c 
+      cell.index = c
       let symbol = document.createElement("p")
-      //cell.addEventListener("mouseover", colorSquare)
       cell.style.border = "2px solid black"
       container.appendChild(cell).className = "grid-item";
       cell.appendChild(symbol).className = "symbol"
       cell.addEventListener('click',handleClick, { once: true})
-    };
+      
+    }
+    cellElements = document.querySelectorAll(".grid-item")
+    
 };
+
+
+
+
 makeRows(3, 3);
 let circleTurn
 
+
+
+
 function handleClick(e) {
-    console.log(this.id)
-//placeMark
-const cell = e.target
-const currentPlayer =  circleTurn ? player1.symbol : player2.symbol
-placeMark(cell, currentPlayer)
-//Check For Win
-//Check for Draw
-//Switch Turns
-switchTurns()
+    console.log(this.index)
+    //placeMark
+    
+    const cell = e.target
+    const currentPlayer =  circleTurn ? player1.symbol : player2.symbol
+    placeMark(cell, currentPlayer)
+    //if(checkWin(currentPlayer)){
+   //     console.log("winner")
+   // }
+   if(checkWin(currentPlayer)){
+     if(currentPlayer == player1.symbol) {
+        winningMessage.textContent = "Player X Wins!"
+     } else {
+        winningMessage.textContent = "Player O Wins!"
+     }
+    }
+    //Check For Win
+    //Check for Draw
+    //Switch Turns
+    switchTurns()
+    
 }
 
 function switchTurns(){
@@ -51,8 +90,23 @@ function switchTurns(){
 
 function placeMark(cell, currentPlayer){
     cell.textContent = currentPlayer
+    //gameBoardObject.gameBoard.push(cell.id)
+    //console.log(gameBoardObject.gameBoard)
+    gameBoardObject.gameBoard[cell.id]=currentPlayer;
+    console.log(gameBoardObject.gameBoard);   
    
 }
 
 
 
+
+
+function checkWin(currentPlayer) {
+    return WINNING_COMBINATIONS.some(combination => {
+      return combination.every(index => {
+        return gameBoardObject.gameBoard[index] == currentPlayer
+      })
+    })
+}
+
+const winningMessage = document.querySelector(".winning-message")
